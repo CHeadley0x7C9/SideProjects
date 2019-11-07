c7f3b4c1c4783c771a1c3bb36d29ab602fadd998
CHeadley0x7C9 <54729393+CHeadley0x7C9@users.noreply.github.com>
2019-11-07 13:34:34 -0600

    Update to howold.py
    I wrapped the function in a try/catch statement.

--- howold.py
+++ howold.py
@@ -1,28 +1,31 @@
 
 
-
 def howold(year):
 	'''
 	I wanted to create a function to calculate my age based off of 
-	the year I added as an argument.
+	the year I added as an argument. 
 	'''
+	try:
+		currentage = 26
+		currentyear = 2019
+		if (year > currentyear):
+			add = year - currentyear
+			futureage = currentage + add
+			print("In the year {} I will be {}".format(year,futureage))
+			print("\n")
+		elif(year < currentyear):
+			subtract = currentyear-year
+			pastage = currentage-subtract
+			print("In the year {} I was {}".format(year,pastage))
+			print("\n")
+	except TypeError:
+		print("ERROR: Please use a year as an argument!")
 	
-	currentage = 26
-	currentyear = 2019
-	if (year > currentyear):
-		add = year - currentyear
-		futureage = currentage + add
-		print("In the year {} I will be {}".format(year,futureage))
-		print("\n")
-	elif(year < currentyear):
-		subtract = currentyear-year
-		pastage = currentage-subtract
-		print("In the year {} I was {}".format(year,pastage))
-		print("\n")
 
-#Testing fuction
+#Testing function
 howold(2020)
 howold(2030)
 howold(1992)
 howold(1988)
 howold(3001)
+howold("pink")
